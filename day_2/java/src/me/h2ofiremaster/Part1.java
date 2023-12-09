ed4618bf1e30843d9b5d0ee2ac1a3f07eee670e2
H2ofiremaster <boomertnt628@gmail.com>
2023-12-08 23:32:34 -0800

    java 2-1: Finised day 2-1 after finally tracking down an accidentally flipped condition

--- day_2/java/src/me/h2ofiremaster/Part1.java
+++ day_2/java/src/me/h2ofiremaster/Part1.java
@@ -2,14 +2,10 @@ package me.h2ofiremaster;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Part1 {
-    private static final File INPUT_FILE = new File("example.txt");
+    private static final File INPUT_FILE = new File("input.txt");
     
     private static final int SOLUTION_RED = 12;
     private static final int SOLUTION_GREEN = 13;
@@ -23,60 +19,66 @@ public class Part1 {
             System.err.println("File '" + INPUT_FILE + "' was not found.");
             return;
         }
-        int output = 0;
     
         LinkedList<GameRound> rounds = new LinkedList<>();
         while(fileScanner.hasNextLine()) {
             String line = fileScanner.nextLine();
-            rounds.add(GameRound.fromString(line));
+            GameRound.fromString(line).ifPresentOrElse(
+                    rounds::add,
+                    () -> System.out.println("'" + line + "' failed to deserialize"));
         }
-        
-        rounds.stream().filter(g -> {
-            return
-        })
+        int output = rounds.stream().filter(round -> SOLUTION_RED >= round.getMax(CubeColor.RED) &&
+                SOLUTION_GREEN >= round.getMax(CubeColor.GREEN) &&
+                SOLUTION_BLUE >= round.getMax(CubeColor.BLUE))
+                .mapToInt(r -> r.id).sum();
         
         System.out.println("Output: " + output);
     }
+    
     
     
     private enum CubeColor {
         RED, GREEN, BLUE
     }
     
-    private record GameRound(int id, List<CubeColor> cubeColors, List<Integer> cubeCounts) {
-        private static Pattern WHITESPACE_TRIM_REGEX = Pattern.compile("");
-        public static GameRound fromString(String string) {
+    private record GameRound(int id, List<CubeStack> stacks) {
+        public static Optional<GameRound> fromString(String string) {
             String[] colonSplit = string.split(":");
-            if(colonSplit.length != 2) throw new IllegalArgumentException("Could not deserilize '" + string + "': More/less than one ':'");
+            if(colonSplit.length != 2) {
+                System.out.println("Could not deserilize '" + string + "': More/less than one ':'");
+                return Optional.empty();
+            }
             String idString = colonSplit[0].substring(5);
-            String[] commaSplit = string.split("[,;]");
-            List<CubeColor> colorList = new LinkedList<>();
-            List<Integer> countList = new LinkedList<>();
-            for(String cubeString : commaSplit) {
-                String[] parts = cubeString.split(" ");
-                
-            }
-        }
-        private static int parseCount(String string) {
-            String countString = string.replaceAll("^\\s*|\\s*$", "");
-            try {
-                return Integer.parseInt(countString);
-            } catch(NumberFormatException e) {
+            String[] commaSplit = colonSplit[1].split("[,;]");
             
+            List<CubeStack> stacks = new LinkedList<>();
+            int id;
+            try {
+                id = Integer.parseInt(idString);
+                for(String cubeString : commaSplit) {
+                    stacks.add(parseStack(cubeString));
+                }
+            } catch(IllegalArgumentException e) {
+                System.out.println("Failed to parse string: " + string);
+                return Optional.empty();
             }
+            return Optional.of(new GameRound(id, stacks));
         }
-        private static CubeColor parseColor(String cubeString) {
-        
+        private static CubeStack parseStack(String string) throws IllegalArgumentException {
+            String[] cubeStrings = string.replaceAll("^\\s*|\\s*$", "").split(" ");
+            String countString = cubeStrings[0];
+            String colorString = cubeStrings[1].toUpperCase();
+            return new CubeStack(Integer.parseInt(countString), CubeColor.valueOf(colorString));
         }
     
         public int getMax(CubeColor color) {
             int result = 0;
-            for(int i = 0; i < cubeColors.size(); i++) {
-                if(cubeColors.get(i) != color) continue;
-                result = Math.max(result, cubeAmounts.get(i));
+            for(CubeStack stack : stacks) {
+                if(stack.color != color) continue;
+                result = Math.max(result, stack.count);
             }
             return result;
         }
-    
+        record CubeStack(int count, CubeColor color) {}
     }
 }
